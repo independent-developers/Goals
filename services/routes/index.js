97c8e82f00b3d0a3fcc143009e982e20ebac3009
Ugo Arzur <ugo.arzur@gmail.com>
2019-07-24 23:37:41 +0200

    refactor route

--- services/routes/index.js
+++ services/routes/index.js
@@ -14,19 +14,14 @@ const routes = [
 		handler: users.create,
 	},
 	{
+		method: 'DELETE',
+		path: '/users/{userId?}',
+		handler: users.remove,
+	},
+	{
 		method: 'POST',
 		path: '/users/{userId}/goals',
 		handler: goals.create,
-	},
-	{
-		method: 'DELETE',
-		path: '/users',
-		handler: users.remove,
-	},
-	{
-		method: 'DELETE',
-		path: '/users/{userId}',
-		handler: users.remove,
 	},
 	{
 		method: 'DELETE',
