6598a11d84d20287682abe830a6e6a87c4657031
Ugo Arzur <ugo.arzur@gmail.com>
2019-07-25 23:30:44 +0200

    wip lambdas

--- api/goals/index.js
+++ api/goals/index.js
@@ -1,4 +1,21 @@
 module.exports = (req, res) => {
-	res.status(200)
-	res.json(req)
+	const { headers, url, method, statusCode, cookies, query, body } = req
+	console.log({ headers, url, method, statusCode, cookies, query, body })
+	switch (req.method) {
+		case 'POST':
+			res.send('POST')
+			break
+		case 'GET':
+			res.send('GET')
+			break
+		case 'PUT':
+			res.send('PUT')
+			break
+		case 'DELETE':
+			res.send('DELETE')
+			break
+
+		default:
+			break
+	}
 }
