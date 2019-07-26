3026dc780c1d68a2d2dcd442d38d9f3c6c29a66b
Ugo Arzur <ugo.arzur@gmail.com>
2019-07-26 21:41:46 +0200

    Manager

--- public/js/elements_manager/elements_manager.js
+++ public/js/elements_manager/elements_manager.js
@@ -1,6 +1,83 @@
 const twitch = window.Twitch.ext;
+const BASE_URL = 'http://localhost:3000';
+
+/**
+ * Getch the streamer's goals list
+ * @param {Integer} streamerId 
+ */
+function fetchGoals(streamerId){
+    return fetch(`${BASE_URL}/users/${streamerId}/goals`)
+        .then(response => response.json())
+        .then(goals => console.log(goals))
+        .catch(error => {
+            console.error('An error occurred while fetching goals', error)
+            throw error
+        })
+}
 
 
+function setGoals(streamerId, goals = []) {
+    
+    // const formattedGoals = goals.reduce((goals, currentGoal) => {
+    //     const key = `${currentGoal.key}`
+    //     goals.push({
+    //         key : {
+    //             title: currentGoal.title,
+    //             checked: currentGoal.check
+    //         }
+    //     })
+    //     return goals;
+    //   }, []);
+    //   console.log('formattedGoals', formattedGoals)
+    goals.forEach(goal => {
+        let url = "";
+        if(goal && goal.key){
+            url = `${BASE_URL}/users/${streamerId}/goals/${goal.key}`
+        } else {
+            url = `${BASE_URL}/users/${streamerId}/goals`
+        }
+        return fetch(url, {
+                method: 'POST', // *GET, POST, PUT, DELETE, etc.
+                mode: 'cors', // no-cors, cors, *same-origin
+                cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+                credentials: 'same-origin', // include, *same-origin, omit
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    title: goal.title,
+                    checked: goal.check
+                }), // body data type must match "Content-Type" header
+            })
+            .then(response => response.json()) // parses JSON response into native JavaScript objects 
+            .catch(error => {
+                console.error('An error occurred while setting some goals', error)
+                throw error
+            });
+    })
+  }
+
+
+const manager = {
+    goals: {
+        fetch: fetchGoals,
+        create: setGoals
+    }
+}
+
+// TODO: Utiliser le manager comme suit pour fetch
+// manager.goals.fetch('1234')
+// TODO: Utiliser le manager comme suit pour create
+// manager.goals.create('1234', [{
+//     "key": "0da0d92183921efe233c79d3ca6ab5bd276641ee",
+//     "title": "Blblblblbl",
+//     "check": true
+// },
+// {
+//     "key": "34c55d8715ad3f95183e2aca5c2e4c4ac2837671",
+//     "title": "Blblblblbl",
+//     "check": false
+// }])
 
 // Model
 var index = 1;
